f527a75ad895b0b636bab6369a56c3075d3d4858
JustDilex <88290149+JustDilex@users.noreply.github.com>
2022-08-09 14:48:04 +0600

    Add files via upload

---
+++ хз.js
@@ -0,0 +1,43 @@
+function plus(){
+    var num1, num2, result;
+    num1 = document.getElementById("num1").value;
+    num1 = parseInt(num1);
+    num2 = document.getElementById("num2").value;
+    num2 = parseInt(num2);
+    
+    result = num1 + num2;
+
+    document.getElementById('out').innerHTML = 'Результат: ' + result;
+}
+function minus(){
+    var num1, num2, result;
+    num1 = document.getElementById("num1").value;
+    num1 = parseInt(num1);
+    num2 = document.getElementById("num2").value;
+    num2 = parseInt(num2);
+
+    result = num1 - num2;
+    document.getElementById('out').innerHTML = 'Результат: ' + result;
+}
+
+function multiply(){
+    var num1, num2, result;
+    num1 = document.getElementById("num1").value;
+    num1 = parseInt(num1);
+    num2 = document.getElementById("num2").value;
+    num2 = parseInt(num2);
+
+    result = num1 * num2;
+    document.getElementById('out').innerHTML = 'Результат: ' + result;
+}
+
+function devide(){
+    var num1, num2, result;
+    num1 = document.getElementById("num1").value;
+    num1 = parseInt(num1);
+    num2 = document.getElementById("num2").value;
+    num2 = parseInt(num2);
+
+    result = num1 / num2;
+    document.getElementById('out').innerHTML = 'Результат: ' + result;
+}
